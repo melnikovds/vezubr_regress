094ed86955c717035c286835e00d3737eac6ad28
dimmycore <flamekh@gmail.com>
2024-12-28 11:33:37 +0300

    25/12

--- pages/manual_page.py
+++ pages/manual_page.py
@@ -6,10 +6,10 @@ class Manual(Base):
         super().__init__(driver)
         self.driver = driver
 
-        filter_date_create = {
-            "xpath": "//div[@class='ant-select-selection__rendered']",
-            "name": "filter_date_create",
-            "reference_xpath": "//div[text()='За год']",
-            "reference": "За год"
-        }
+    filter_date_create = {
+        "xpath": "//div[@class='ant-select-selection__rendered']",
+        "name": "filter_date_create",
+        "reference_xpath": "//div[text()='За год']",
+        "reference": "За год"
+    }
 

ccbd346aa813e6d064ede02c857d6eb3e066a465
dimmycore <flamekh@gmail.com>
2025-02-11 20:52:59 +0300

    11.02

--- pages/filter_old_ftl_page.py
+++ pages/filter_old_ftl_page.py
@@ -1,4 +1,5 @@
 from base.base_class import Base
+import requests
 
 
 class OldFTL(Base):
@@ -12,3 +13,49 @@ class OldFTL(Base):
         "name": "custom_field",
         "reference_xpath": "(//a[@class='vz-tabs-modern__item'])[3]"
     }
+
+
+
+    #  Methods
+    """Create base Old FTL"""
+    def add_old_ftl_lkz(self, api_base_url, api_login) -> str:
+        """
+        Автоматизирует создание старых FTL-рейсов
+
+        Parameters
+        ----------
+        Нет входных параметров. Все необходимые данные генерируются или выбираются внутри метода.
+
+        Returns
+        -------
+        str
+            Уникальный номер созданного рейса. Побочные эффекты: изменения на веб-странице.
+        """
+
+        # логинимся под ролью ЛКЗ
+        token = api_login("lkz")
+
+        # эндпоинт для тестирования
+        url = f"{api_base_url}/v1/api/order/transport-request/create-and-publish"
+
+        # выводим эндпоинт в консоль
+        print(f"Request URL: {url}")
+
+        headers = { "Authorisation": token }
+
+        body = {}
+
+
+
+        res = requests.post(url, headers=headers, params=body)
+
+
+
+
+
+
+
+
+
+
+
